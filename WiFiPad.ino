10c202bcea9bd6d759fe45121854e0732f09c2a0
masutokw <masutokw@users.noreply.github.com>
2022-11-11 19:31:37 +0100

    Focuser Support
    1  focus  out
    3 focus in
    5 select speed fast/slow
    7 ligth on/off

--- WiFiPad.ino
+++ WiFiPad.ino
@@ -1,8 +1,8 @@
 // I2C_KEYPAD: para usar un GPIO expander PCF8574
 // se usa la libreria:
 // https://github.com/joeyoung/arduino_keypads/tree/master/Keypad_I2C
-// si se comenta el #define, se usa el mapeo directo del keypad a los pines del Wemos 
-#define I2C_KEYPAD
+// si se comenta el #define, se usa el mapeo directo del keypad a los pines del Wemos
+//#define I2C_KEYPAD
 
 #include <ESP8266WiFi.h>
 #include <Wire.h>
@@ -11,8 +11,8 @@
 //LiquidCrystal_I2C lcd(0x27, 16, 2);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-#define SDA_PIN D2 
-#define SCL_PIN D1 
+#define SDA_PIN D2
+#define SCL_PIN D1
 
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
@@ -25,21 +25,21 @@ char keys[ROWS][COLS] = {
 char key;
 
 #ifdef I2C_KEYPAD
-  #include "Keypad_I2C.h"
-  #define I2CADDR 0x20
-  byte rowPins[ROWS] = {0, 1, 2, 3}; //connect to the row pinouts of the keypad
-  byte colPins[COLS] = {4, 5, 6}; //connect to the column pinouts of the keypad
-  Keypad_I2C keypad = Keypad_I2C( makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR );
+#include "Keypad_I2C.h"
+#define I2CADDR 0x20
+byte rowPins[ROWS] = {0, 1, 2, 3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {4, 5, 6}; //connect to the column pinouts of the keypad
+Keypad_I2C keypad = Keypad_I2C( makeKeymap(keys), rowPins, colPins, ROWS, COLS, I2CADDR );
 #else
-  byte rowPins[ROWS] = {D7, D6, D5, D0} ;
-  byte colPins[COLS] = { D8, D4, D3} ;
-  #include <Keypad.h>
-  Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+byte rowPins[ROWS] = {D7, D6, D5, D0} ;
+byte colPins[COLS] = { D8, D4, D3} ;
+#include <Keypad.h>
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 #endif
 
 #include <Ticker.h>
 Ticker tickerKey;
-#define AP
+//#define AP
 #ifndef AP
 #define STASSID "MYSSID"
 #define STAPSK  "MYPASS"
@@ -54,8 +54,8 @@ const char* host = "192.168.4.1";
 WiFiClient client;
 const char* ssid     = STASSID;
 const char* password = STAPSK;
-
-
+bool bklight = true;
+bool focusF = false;
 const uint16_t port = 10001;
 void InitOTA()
 { ArduinoOTA.setHostname("wifipad.local");
@@ -82,12 +82,22 @@ void keypadEvent(KeypadEvent key) {
         switch  (key) {
           case '4': client.print(":Mw#"); lcd.print("West  "); break;
           case '6': client.print(":Me#"); lcd.print("East  ") ; break;
+          case '1': client.print(":F+#"); lcd.print("Focus+") ; break;
+          case '3': client.print(":F-#"); lcd.print("Focus-") ; break;
           case '2': client.print(":Mn#"); lcd.print("North "); break;
           case '8': client.print(":Ms#"); lcd.print("South "); break;
           case '#': client.print(":RS#"); lcd.setCursor(11, 1); lcd.print("Slew  "); break;
           case '*': client.print(":RC#"); lcd.setCursor(11, 1); lcd.print("Center"); break;
           case '0':  client.print(":RG#");  lcd.setCursor(11, 1); lcd.print("Guide "); break;
-          case '9':  client.print(":RG#");  lcd.setCursor(11, 1); lcd.print("Find "); break;
+          case '9':  client.print(":RM#");  lcd.setCursor(11, 1); lcd.print("Find "); break;
+          case '7':  lcd.setCursor(0, 2); if (focusF) {
+              client.print(":FF#");
+               lcd.print("FastF ");
+            } else {
+              client.print(":FS#");
+              lcd.print("SlowF ");
+            }; focusF = !focusF; break;
+          case '5': if (bklight) lcd.backlight() ; else lcd.noBacklight(); bklight = !bklight ; break;
           default: break;
         }
 
@@ -102,6 +112,8 @@ void keypadEvent(KeypadEvent key) {
           case '6': client.print(":Qe#"); break;
           case '2': client.print(":Qn#"); break;
           case '8': client.print(":Qs#"); break;
+          case '3': client.print(":FQ#"); break;
+          case '5': client.print(":FQ#"); break;
           default: break;
 
         }
@@ -144,7 +156,7 @@ void setup() {
 
   WiFi.mode(WIFI_STA);
 #ifndef AP
-  IPAddress ip(192, 168, 1, 36);
+  IPAddress ip(192, 168, 1, 35);
   IPAddress gateway(192, 168, 1, 1);
   IPAddress subnet(255, 255, 0, 0);
   IPAddress DNS(192, 168, 1, 1);
@@ -171,7 +183,7 @@ void setup() {
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("ESP32go Wctrl");
+  lcd.print("ESP32go WifiPad");
   lcd.setCursor(0, 1);
   lcd.print(WiFi.localIP());
   delay(3000);
@@ -274,9 +286,8 @@ void loop() {
 
 
 /*   keypad
-   col   1 NC  2 GPIO13  3 GPIO12 4 GPIO14 5 GPIO16 
-   row   6 GPIO0 7 GPIO02 8 GPIO15 
+   col   1 NC  2 GPIO13  3 GPIO12 4 GPIO14 5 GPIO16
+   row   6 GPIO0 7 GPIO02 8 GPIO15
    lcd
-   
- */    
- 
+
+*/
